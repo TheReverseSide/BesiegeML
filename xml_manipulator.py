a3174e8db242b7d1f24185183eac52fcf5a3ba1c
Brandon Startz <brandon.startz@gmail.com>
2020-03-08 18:45:04 +0200

    implement xml manipulator

--- xml_manipulator.py
+++ xml_manipulator.py
@@ -1,7 +1,7 @@
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET\
 
-tree = ET.parse('/Users/startz/Library/Application Support/Steam/steamapps/common/Besiege/Besiege.app/Contents/SavedMachines/train_0.bsg')
-root = tree.getroot()
+
+PATH = "d:\\Games\\steamapps\\common\\Besiege\\Besiege_Data\\SavedMachines\\"
 
 
 #* READING
@@ -10,13 +10,13 @@ root = tree.getroot()
 # root[0][0].attrib['x'] = '3'
 # print(root[0][0].attrib['x'])
 
-tree.write('/Users/startz/Library/Application Support/Steam/steamapps/common/Besiege/Besiege.app/Contents/SavedMachines/train_0_2.bsg')
-# add this line "<?xml version="1.0" encoding="utf-8"?>"
+# tree.write('/Users/startz/Library/Application Support/Steam/steamapps/common/Besiege/Besiege.app/Contents/SavedMachines/train_0_2.bsg')
+# # add this line "<?xml version="1.0" encoding="utf-8"?>"
 
-with open('/Users/startz/Library/Application Support/Steam/steamapps/common/Besiege/Besiege.app/Contents/SavedMachines/train_0_2.bsg', 'r+') as f:
-    content = f.read()
-    f.seek(0, 0)
-    f.write('<?xml version="1.0" encoding="utf-8"?>' + '\n' + content)
+# with open('/Users/startz/Library/Application Support/Steam/steamapps/common/Besiege/Besiege.app/Contents/SavedMachines/train_0_2.bsg', 'r+') as f:
+#     content = f.read()
+#     f.seek(0, 0)
+#     f.write('<?xml version="1.0" encoding="utf-8"?>' + '\n' + content)
 
 # Global rotation
 # print('\n Global rotation:')j
@@ -45,3 +45,58 @@ Save score to something
 Run for 10 min
 Save best to a specific file
 '''
+
+def print_save(save_obj):
+    print(f"print_save({save_obj})")
+    for child in save_obj.getroot().iter():
+        print(child.tag, child.attrib)
+
+
+def save_as_xml(save_obj):
+    print(f"save_as_xml({save_obj})")
+    filename = save_obj.getroot().attrib['name']
+    full_filename = PATH + f"{filename}.bsg"
+    save_obj.write(full_filename)
+
+    #add header
+    with open(full_filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write('<?xml version="1.0" encoding="utf-8"?>' + '\n\n' + content)
+
+
+def manipulate_save_object(save_obj):
+    print(f"manipulate_save_object({save_obj})")
+    # print(save_obj.getroot().attrib['name'])
+    root = save_obj.getroot()
+    root.attrib['name'] = "xml_generated"
+    
+    # Change position of block with ID 15
+    for block in root.iter('Block'):
+        
+        if block.attrib['id'] == '15':
+            pos = next(block.iter('Position'))
+            print(pos.tag, pos.attrib)
+            pos.attrib['y'] = '0.5'
+            pos.attrib['z'] = '0'
+    
+    
+    # root.attrib['Blocks'] = 
+
+
+def get_save_object(save_name):
+    print(f"get_save_object({save_name})")
+    tree = ET.parse(PATH + f"{save_name}.bsg")
+    root = tree.getroot()
+    # print(root.attrib['name'])
+    # print_save(root)
+    return tree
+
+def main():
+    save_obj = get_save_object('xml_original')
+    manipulate_save_object(save_obj)
+    save_as_xml(save_obj)
+
+
+if __name__ == "__main__":
+    main()
